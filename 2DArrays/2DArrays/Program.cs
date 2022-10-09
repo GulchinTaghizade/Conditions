b8a9a73784bad5a1ee661c15ba84a1d55a679876
GulchinTaghizade <100687270+GulchinTaghizade@users.noreply.github.com>
2022-10-09 23:22:19 +0400

    Add files via upload

---
+++ 2DArrays/2DArrays/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace _2DArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task1
+            //1.Find out minimal element.
+            //static void MinElement(int[ , ] arr)
+            //{
+            //    int minValue = arr[0, 0];
+            //    for (int i = 0; i < arr.GetLength(0); i++)
+            //    {
+            //        for (int j = 0; j < arr.GetLength(1); j++)
+            //        {
+            //            if (arr[i, j] < minValue)
+            //            {
+            //                minValue = arr[i, j];
+            //            }
+            //        }
+            //    }
+            //    Console.WriteLine(minValue);
+            //}
+            //int[,] arr = new int[,] { { 1, 2 }, { 3, 4 }, { 5, 6 }, { 7, 8 }, { 9, 10 } };
+            //MinElement(arr);
+            #endregion
+
+            #region Task2
+            //2.Find out maximal element.
+            //static void MaxElement(int[,] arr)
+            //{
+            //    int maxValue = arr[0, 0];
+            //    for (int i = 0; i < arr.GetLength(0); i++)
+            //    {
+            //        for (int j = 0; j < arr.GetLength(1); j++)
+            //        {
+            //            if (arr[i, j] > maxValue)
+            //            {
+            //                maxValue = arr[i, j];
+            //            }
+            //        }
+            //    }
+            //    Console.WriteLine(maxValue);
+            //}
+            //int[,] arr = new int[,] { { 1, 2 }, { 3, 4 }, { 5, 6 }, { 7, 8 }, { 9, 10 } };
+            //MaxElement(arr);
+            #endregion
+
+            #region Task3
+            //3.Find out index of maximal element.
+            //static void MaxElementIndex(int[,] arr)
+            //{
+            //    int maxValue = arr[0, 0];
+            //    int maxValueIndex1 = 0;
+            //    int maxValueIndex2 = 0;
+            //    for (int i = 0; i < arr.GetLength(0); i++)
+            //    {
+            //        for (int j = 0; j < arr.GetLength(1); j++)
+            //        {
+            //            if (arr[i, j] > maxValue)
+            //            {
+            //                maxValue = arr[i, j];
+            //                maxValueIndex1 = i;
+            //                maxValueIndex2 = j;
+            //            }
+            //        }
+            //    }
+            //    Console.WriteLine($"[{maxValueIndex1},{maxValueIndex2}]");
+            //}
+            //int[,] arr = new int[,] { { 1, 2 }, { 3, 4 }, { 5, 6 }, { 7, 8 }, { 9, 10 } };
+            //MaxElementIndex(arr);
+            #endregion
+
+            #region Task4
+            //3.Find out index of minimal element.
+            //static void MinElementIndex(int[,] arr)
+            //{
+            //    int minValue = arr[0, 0];
+            //    int minValueIndex1 = 0;
+            //    int minValueIndex2 = 0;
+            //    for (int i = 0; i < arr.GetLength(0); i++)
+            //    {
+            //        for (int j = 0; j < arr.GetLength(1); j++)
+            //        {
+            //            if (arr[i, j] < minValue)
+            //            {
+            //                minValue = arr[i, j];
+            //                minValueIndex1 = i;
+            //                minValueIndex2 = j;
+            //            }
+            //        }
+            //    }
+            //    Console.WriteLine($"[{minValueIndex1},{minValueIndex2}]");
+            //}
+            //int[,] arr = new int[,] { { 1, 2 }, { 3, 4 }, { 5, 6 }, { 7, 8 }, { 9, 10 } };
+            //MinElementIndex(arr);
+            #endregion
+
+        
+    }
+    }
+}
+
