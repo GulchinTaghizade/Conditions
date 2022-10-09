f88a0aac392409bd85563b8041ce9c4234b59d1d
GulchinTaghizade <100687270+GulchinTaghizade@users.noreply.github.com>
2022-10-09 20:24:19 +0400

    Update Program.cs

--- Cycles/Cycles/Program.cs
+++ Cycles/Cycles/Program.cs
@@ -323,6 +323,261 @@ namespace Cycles
             //}
            
             #endregion
+                
+                #region Task15
+            //User gives positive n. Calculate:
+            //1)
+            //Console.WriteLine("Please enter positive number n:");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //double product = 1;
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    product *= (2 + (1.0 / Factorial(i)));
+
+            //}
+
+            //Console.WriteLine(product);
+
+
+
+            //2)
+            //Console.WriteLine("Please enter positive number n:");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //double sum = 0;
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    sum += ((1 + i) / Factorial(i));
+
+            //}
+
+            //Console.WriteLine(sum);
+
+            //static int Factorial(int n)
+            //{
+            //    int fact = 1;
+            //    for (int i = 1; i <= n; i++)
+            //    {
+            //        fact = fact * i;
+            //    }
+            //    return fact;
+            //}
+
+
+
+            #endregion
+
+            #region Task16
+            //Calculate the approximate value of an infinite sum. After equls sign you see it’s exact value for you to compare with your result.:
+            //Console.WriteLine("Input number d:");
+            //int d = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Input number x:");
+            //int x = Convert.ToInt32(Console.ReadLine());
+
+            //static void checkInaccuracy(int n)
+            //{
+            //    double result = Math.Pow(Math.PI, 2) / 6;
+            //    double sum = 0;
+            //    for (int i = 1; i <= n; i++)
+            //    {
+            //        sum += 1.0 / (i * i);
+            //    }
+
+            //    Console.WriteLine($"Inaccuracy in this calculation is {result - sum}");
+            //}
+            //checkInaccuracy(n);
+
+            //static void checkInaccuracy(int n)
+            //{
+            //    double result = 3.0 / 4;
+            //    double sum = 0;
+            //    for (int i = 1; i <= n; i++)
+            //    {
+            //        sum += 1.0 / (i * (i+2));
+            //    }
+
+            //    Console.WriteLine($"Inaccuracy in this calculation is {result - sum}");
+            //}
+            //checkInaccuracy(n);
+
+            //static void checkInaccuracy(int d,int x)
+            //{
+            //    double result = Math.Pow(Math.E, x) ;
+            //    int factorial = 1;
+            //    double sum = 1;
+            //    for (int i = 1; i <= d; i++)
+            //    {
+            //        factorial *= i;
+            //        sum += Math.Pow(x,i) / factorial;
+            //    }   
+
+            //    Console.WriteLine($"Inaccuracy in this calculation is {result - sum}");
+            //}
+            //checkInaccuracy(d,x);
+            #endregion
+
+            #region Task17
+            //17.Find out all 2-digits numbers, whose digit’s sum doesn’t change after multiplying this number by 2,3,4,5,6,7,8,9.
+            //for (int i = 10; i <= 99; i++)
+            //{
+            //    for (int j = 2; j <= 9; j++)
+            //    {
+            //        if (SumOfDigits(i * j) == SumOfDigits(i))
+            //        {
+            //            Console.WriteLine($"{i}---{j}");
+            //        }
+            //        else
+            //        {
+            //            break;
+            //        }
+            //    }
+            //}
+
+
+            //static int SumOfDigits(int n)
+            //{
+            //    int sum = 0;
+            //    int temp = n;
+            //    int digit = 0;
+            //    while (temp > 0)
+            //    {
+            //        digit = temp % 10;
+            //        sum += digit;
+            //        temp = (temp - digit) / 10;
+            //    }
+            //    return sum;
+            //}
+
+            #endregion
+
+            #region Task18
+            //18.Find out every 3-digit number that can be represented as sum of all its digit’s factorials.
+            //static int Factorial(int n)
+            //{
+            //    int fact = 1;
+            //    for (int i = 1; i <= n; i++)
+            //    {
+            //        fact = fact * i;
+            //    }
+            //    return fact;
+            //}
+
+            //int sum = 0;
+            //for (int i = 100; i <= 999; i++)
+            //{
+            //    int temp = i;
+            //    while (temp > 0)
+            //    {
+            //        int currentDigit = temp % 10;
+            //        int factorialDigit = Factorial(currentDigit);
+            //        sum += factorialDigit;
+            //        temp = (temp - currentDigit) / 10;
+            //    }
+            //    if (i == sum)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //    sum = 0;
+            //}
+            #endregion
+
+            #region Task19
+            //19.Is it possible to represent given number as sum of two positive numbers powered to 2? 
+            //Console.WriteLine("Please enter the number n:");
+            //int n = Convert.ToInt32(Console.ReadLine());
+
+            //static void checkSquare(int n)
+            //{
+            //    for (int i = 1; i * i < n; i++)
+            //    {
+            //        for (int j = i; j * j < n; j++)
+            //        {
+            //            if ((i * i) + (j * j) == n)
+            //            {
+            //                Console.WriteLine($"It is possible to represent given number as sum of {i} and {j} powered to 2");
+            //            }
+            //        }
+            //    }
+            //}
+            //checkSquare(n);
+            #endregion
+
+            #region Task20
+            //20.Find out is given number perfect? I.e. number is equal to sum of all it’s positive divisors excepting the number by it’s own.
+
+            //Console.Write("Please enter number: ");
+            //int number = Convert.ToInt32(Console.ReadLine());
+            //int sum = 0;
+            //for(int i=1; i < number; i++)
+            //{
+            //    if(number % i == 0)
+            //    {
+            //        sum += i;
+            //    }
+            //}
+            //if(sum == number)
+            //{
+            //    Console.WriteLine("Perfect number");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Not perfect number");
+            //}
+            #endregion
+
+            #region Task 21
+            //21.User gives one positive number k. Find out k-th digit of series: 1234567891011121314… which have every positive numbers one by one.
+            //static void GetDigit(int k)
+            //{
+            //    string numbers = "";
+            //    for (int i = 1; i <= k; i++)
+            //    {
+            //        numbers += i;
+            //    }
+            //    Console.WriteLine(numbers[k-1]);
+            //}
+            //GetDigit(10);
+            #endregion
+
+            #region Task 22
+            //22.User gives one positive number k. Find out k-th digit of series: 149162536… which have every positive numbers one by one powered by 2.
+            //static void GetDigit(int k)
+            //{
+            //    string nums = "";
+            //    for (int i = 1; i <= k; i++)
+            //    {
+            //        nums += i * i;
+            //    }
+            //    Console.WriteLine(nums[k-1]);
+            //}
+            //GetDigit(5);
+            #endregion
+
+            #region Task 23
+            //23.User gives one positive number k. Find out k-th digit of Fibonacci’s series: 112358132134…
+
+            //static void GetDigit(int k)
+            //{
+            //    string nums = "";
+            //    int num1 = 1;
+            //    int num2 = 1;
+            //    nums += num1;
+            //    nums += num2;
+
+            //int[] fibonacciNums = new int[k];
+            //fibonacciNums[0] = num1;
+            //fibonacciNums[1] = num2;
+            //for (int i = 2; i < fibonacciNums.Length; i++)
+            //{
+            //    fibonacciNums[i] = num1 + num2;
+            //    num1 = fibonacciNums[i - 1];
+            //    num2 = fibonacciNums[i];
+            //    nums += fibonacciNums[i];
+
+            //}
+            //Console.WriteLine(nums[k - 1]);
+            //}
+            //GetDigit(7);
+        #endregion
         }
     }
 }
