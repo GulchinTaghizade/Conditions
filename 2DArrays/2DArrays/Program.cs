0a5395a6ddfdfa6d123de94b10500e43c671356a
GulchinTaghizade <100687270+GulchinTaghizade@users.noreply.github.com>
2022-10-11 23:25:21 +0400

    Update Program.cs

--- 2DArrays/2DArrays/Program.cs
+++ 2DArrays/2DArrays/Program.cs
@@ -97,6 +97,77 @@ namespace _2DArrays
             //int[,] arr = new int[,] { { 1, 2 }, { 3, 4 }, { 5, 6 }, { 7, 8 }, { 9, 10 } };
             //MinElementIndex(arr);
             #endregion
+               
+             #region Task5
+            //5.Find out amount of elements that are bigger than every it’s neighbor.
+            //static void AmountOfBiggerElementsThanNeighbors(int[,] arr)
+            //{
+            //    int count = 0;
+            //    for (int i = 1; i < arr.GetLength(0)-1; i++)
+            //    {
+
+            //        for (int j = 1; j < arr.GetLength(1)-1; j++)
+            //        {
+            //            if (i==0 || j==0)
+            //            {
+
+            //            }
+            //            Console.WriteLine(arr[i, j]);
+            //            if (arr[i, j] > arr[i+1,j] && arr[i, j] > arr[i,j+1] && arr[i, j] > arr[i-1,j] && arr[i, j] > arr[i,j-1])
+            //            {
+            //                count++;
+            //                //Console.WriteLine(arr[i,j]);
+            //            }
+            //        }
+            //    }
+            //    //Console.WriteLine(count);
+            //}
+            //int[,] arr = new int[,] {  { 1, 2 ,6},
+            //                           { 3, 4 ,9},
+            //                           { 5, 99 ,34},
+            //                           { 7, 8 ,65},
+            //                           { 9, 10,3 } };
+            //AmountOfBiggerElementsThanNeighbors(arr);
+            #endregion
+
+
+            #region Task12
+            //Console.Write("Enter your number:");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //int[,] arr = new int[n, n];
+            //int printValue = 1;
+            //int c1 = 0, c2 = n - 1;
+            //while (printValue <= n * n)
+            //{
+            //    //Right Direction Move  
+            //    for (int i = c1; i <= c2; i++)
+            //        arr[c1, i] = printValue++;
+
+            //    //Down Direction Move  
+            //    for (int j = c1 + 1; j <= c2; j++)
+            //        arr[j, c2] = printValue++;
+
+            //    //Left Direction Move  
+            //    for (int i = c2 - 1; i >= c1; i--)
+            //        arr[c2, i] = printValue++;
+
+            //    //Up Direction Move  
+            //    for (int j = c2 - 1; j >= c1 + 1; j--)
+            //        arr[j, c1] = printValue++;
+            //    c1++;
+            //    c2--;
+            //}
+            //for (int i = 0; i < n; i++)
+            //{
+            //    for (int j = 0; j < n; j++)
+            //    {
+            //        Console.Write(arr[i, j] + "\t");
+            //    }
+            //    Console.WriteLine();
+            //}
+            //Console.Read();
+            #endregion
+
 
         
     }
